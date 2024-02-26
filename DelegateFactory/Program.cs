2e3ce001d377f47b67ed1a683c61658cc1ff83b6
zhuqinfei <1586859762@qq.com>
2024-02-26 11:50:07 +0800

    委托工厂

---
+++ DelegateFactory/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WrapFactory wrapfactory = new WrapFactory();
+            ProductFactory productFactory = new ProductFactory();
+
+            Func<Product> func1 = new Func<Product>(productFactory.MakePisha);
+
+            Box box = wrapfactory.MakeBox(func1);
+
+            Console.WriteLine(box.product.Name);
+
+            
+        }
+    }
+    class Product
+    {
+        public string Name { get; set; }
+    }
+    class Box
+    {
+        public Product product;
+    }
+    class WrapFactory
+    {
+        public Box MakeBox(Func<Product> product)
+        {
+            Box box = new Box();
+            box.product = product.Invoke();
+            return box;
+        }
+
+    }
+    class ProductFactory
+    {
+       public Product MakePisha()
+        {
+            Product product = new Product();
+            product.Name = "pisha";
+            return product;
+        }
+    }
+}
